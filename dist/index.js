467d94428ca7f0a61da9664d169092cc44d9c7cf
arfan36 <arfanur36@gmail.com>
2023-05-01 12:50:55 +0600

    2-1 :Installation typescript , nvm & ts-node-dev

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const course = "Next Level  Course";
+console.log("🚀 ~ course:", course);
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const course: string = "Next Level  Course";
+console.log("🚀 ~ course:", course)

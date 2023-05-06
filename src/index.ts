ea1f99d00079b9baf65949e67d5c15811b40e015
arfan36 <arfanur36@gmail.com>
2023-05-06 15:51:04 +0600

    11-quiz

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 let course: string = "Next Level Course";
-let rollNumber: number =123;
+let rollNumber: number = 123;
 let isAdmin: boolean = true;
-
 
 // Primitive types
 // string
 // number
 // boolean
 // null
-//  undefined
+// undefined
+
+// bigint , symbol

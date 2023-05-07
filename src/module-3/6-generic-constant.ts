3674e58b3b687bb2cdd71d669d0c0965ed09f340
arfan36 <arfanur36@gmail.com>
2023-05-07 20:08:36 +0600

    3-6: Constraints in Generics

---
+++ src/module-3/6-generic-constant.ts
@@ -0,0 +1,33 @@
+// Spread Operator
+
+// type MandatoryDataTypes = { name: string; age: number; salary: number };
+interface MandatoryInterface {
+	name: string;
+	age: number;
+	salary: number;
+}
+
+const addMeInMyCrushMind = <T extends MandatoryInterface>(myInfo: T) => {
+	const crush = "Kate Winslets";
+	const newData = { ...myInfo, crush };
+	return newData;
+};
+
+type MyInfoType = {
+	name: string;
+	age: number;
+	salary: number;
+	other1: boolean;
+	other2: null;
+};
+
+const myInfo = {
+	name: "Persian",
+	age: 100,
+	salary: 1000000,
+	other1: false,
+	other2: null,
+};
+
+const result5 = addMeInMyCrushMind(myInfo);
+// result5

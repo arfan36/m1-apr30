133a40c3e9742390bd999e09162d89736d529374
arfan36 <arfanur36@gmail.com>
2023-05-15 12:52:44 +0600

    4-6: Getters and Setters in Typescript

--- src/module-4/5-access-modifiers.ts
+++ src/module-4/5-access-modifiers.ts
@@ -1,27 +1,27 @@
-class BankAccount {
-	public readonly id: number;
-	public name: string;
-	protected _balance: number;
+// class BankAccount {
+// 	public readonly id: number;
+// 	public name: string;
+// 	protected _balance: number;
 
-	constructor(id: number, name: string, balance: number) {
-		this.id = id;
-		this.name = name;
-		this._balance = balance;
-	}
-	getBalance() {
-		console.log(`My Current Balance: ${this._balance}`);
-	}
-	addDeposit(amount: number) {
-		this._balance += amount;
-	}
-}
+// 	constructor(id: number, name: string, balance: number) {
+// 		this.id = id;
+// 		this.name = name;
+// 		this._balance = balance;
+// 	}
+// 	getBalance() {
+// 		console.log(`My Current Balance: ${this._balance}`);
+// 	}
+// 	addDeposit(amount: number) {
+// 		this._balance += amount;
+// 	}
+// }
 
-class StudentAccount extends BankAccount {
-	test() {
-		// this._balance = 0;
-	}
-}
+// class StudentAccount extends BankAccount {
+// 	test() {
+// 		// this._balance = 0;
+// 	}
+// }
 
-const myAccount = new BankAccount(444, "Persian", 20);
-// myAccount.balance = 0;
-console.log(myAccount);
+// const myAccount = new BankAccount(444, "Persian", 20);
+// // myAccount.balance = 0;
+// console.log(myAccount);

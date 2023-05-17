b1bee25188af775fc1d3d958d3bf64799c7c91d9
arfan36 <arfanur36@gmail.com>
2023-05-17 08:21:07 +0600

    4-10: Encapsulation and module Summary

--- src/module-4/6-getter-setter.ts
+++ src/module-4/6-getter-setter.ts
@@ -1,39 +1,47 @@
-// class BankAccount {
-// 	public readonly id: number;
-// 	public name: string;
-// 	private _balance: number;
+class BankAccount {
+	public readonly id: number;
+	public name: string;
+	private _balance: number;
 
-// 	constructor(id: number, name: string, balance: number) {
-// 		this.id = id;
-// 		this.name = name;
-// 		this._balance = balance;
-// 	}
+	constructor(id: number, name: string, balance: number) {
+		this.id = id;
+		this.name = name;
+		this._balance = balance;
+	}
 
-// 	// getter
-// 	get balance(): number {
-// 		return this._balance;
-// 	}
-// 	// getBalance(): number {
-// 	// 	return this._balance;
-// 	// }
+	private getTestBalance(): number {
+		return this._balance;
+	}
 
-// 	// setter
-// 	set deposit(amount: number) {
-// 		this._balance = this._balance + amount;
-// 	}
-// 	// addDeposit(amount: number) {
-// 	// 	this._balance = this._balance + amount;
-// 	// }
-// }
+	get Test(): number {
+		return this.getTestBalance();
+	}
 
-// class StudentAccount extends BankAccount {
-// 	test() {
-// 		// this._balance = 0;
-// 	}
-// }
+	// getter
+	get balance(): number {
+		return this._balance;
+	}
+	// getBalance(): number {
+	// 	return this._balance;
+	// }
 
-// const myAccount = new BankAccount(444, "Persian", 30);
-// // myAccount.balance = 0;
-// console.log(myAccount.balance);
-// myAccount.deposit = 30;
-// console.log(myAccount.balance);
+	// setter
+	set deposit(amount: number) {
+		this._balance = this._balance + amount;
+	}
+	// addDeposit(amount: number) {
+	// 	this._balance = this._balance + amount;
+	// }
+}
+
+class StudentAccount extends BankAccount {
+	test() {
+		this.Test;
+	}
+}
+
+const myAccount = new BankAccount(444, "Persian", 30);
+// myAccount.balance = 0;
+console.log(myAccount.balance);
+myAccount.deposit = 30;
+console.log(myAccount.balance);
--- src/module-4/9-abstraction.ts
+++ src/module-4/9-abstraction.ts
@@ -10,57 +10,54 @@
 // 	model: "2000",
 // };
 
-// interface IVehicle {
-// 	startEngine(): void;
-// 	stopEngine(): void;
-// 	move(): void;
-// }
+interface Vehicle {
+	startEngine(): void;
+	stopEngine(): void;
+	move?(): void;
+}
 
-// class Vehicle implements IVehicle {
-// 	constructor(
-// 		public name: string,
-// 		public brand: string,
-// 		public model: number
-// 	) {}
-// 	startEngine(): void {
-// 		console.log("I am starting engine");
-// 	}
-// 	stopEngine(): void {
-// 		console.log("I am stopping engine");
-// 	}
-// 	move(): void {
-// 		console.log("I am moving");
-// 	}
-// 	test() {
-// 		console.log("I am for testing purpose");
-// 	}
-// }
-
-// const vehicle = new Vehicle("Car", "Toyota", 2000);
-
-// # abstract class
-
-abstract class Vehicle {
+class Car implements Vehicle {
 	constructor(
 		public name: string,
 		public brand: string,
 		public model: number
 	) {}
-	abstract startEngine(): void;
-	abstract stopEngine(): void;
-	move(): void {
-		console.log("I am moving");
-	}
-}
-
-class Car extends Vehicle {
 	startEngine(): void {
 		console.log("I am starting engine");
 	}
 	stopEngine(): void {
 		console.log("I am stopping engine");
 	}
+	// move(): void {
+	// 	console.log("I am moving");
+	// }
 }
+
+// const vehicle = new Vehicle("Car", "Toyota", 2000);
+
+// # abstract class
+
+// abstract class Vehicle {
+// 	constructor(
+// 		public name: string,
+// 		public brand: string,
+// 		public model: number
+// 	) {}
+// 	abstract startEngine(): void;
+// 	abstract stopEngine(): void;
+// 	move(): void {
+// 		console.log("I am moving");
+// 	}
+// }
+
+// class Car extends Vehicle {
+// 	startEngine(): void {
+// 		console.log("I am starting engine");
+// 	}
+// 	stopEngine(): void {
+// 		console.log("I am stopping engine");
+// 	}
+// }
 
 // const car1 = new Vehicle("Car1", "Honda", 2015);
 // car1.
